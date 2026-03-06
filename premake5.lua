4aac117856110cae5b57664e69219de9cebb0057
0xsmft <beastboioioi.dev@gmail.com>
2026-03-06 17:16:31 +0000

    Add premake file

---
+++ premake5.lua
@@ -0,0 +1,54 @@
+project "JoltPhysics"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++23"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+	{
+		"Jolt/**.cpp",
+		"Jolt/**.h",
+		"Jolt/**.inl",
+		"Jolt/**.gliffy"
+	}
+
+    includedirs 
+    {
+        "Jolt"        
+    }
+
+    fliter "systen:windows"
+        systemversion "latest"
+		staticruntime "off"
+    
+        files { "Jolt/Jolt.natvis" }
+    
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+        defines
+        {
+            "_DEBUG",
+            "JPH_DEBUG_RENDERER",
+            "JPH_FLOATING_POINT_EXCEPTIONS_ENABLED",
+            "JPH_EXTERNAL_PROFILE",
+			"JPH_ENABLE_ASSERTS"
+        }
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+        vectorextensions "AVX2"
+        isaextensions { "BMI", "POPCNT", "LZCNT", "F16C" }
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
+
+        vectorextensions "AVX2"
+        isaextensions { "BMI", "POPCNT", "LZCNT", "F16C" }
